e9d4b3f971a08f1f609b6bba0b5ba2bef34d6f54
kiwianatra <mcshonax@gmail.com>
2026-09-23 21:00:33 +0500

    Update mod.min.user.js

--- mod.min.user.js
+++ mod.min.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         kMax Mod
 // @namespace    http://tampermonkey.net/
-// @version      1.5.4
+// @version      1.5.5
 // @description  Mod for max.ru with extra features: beta tester crown, photo metadata, hiding elements and analytics blocking
 // @author       kiwinatra потемкин короче
 // @match        *://*.max.ru/*
